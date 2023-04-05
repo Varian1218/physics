78b99a5e39e8d503e9c4c0297332670e135caaf3
DESKTOP-UJRR6FS\Admin <tuduchoang@gmail.com>
2023-04-05 22:29:31 +0700

    2.0.0

--- IPhysics.cs
+++ IPhysics.cs
@@ -6,6 +6,7 @@ namespace Physics
 {
     public interface IPhysics
     {
+        IEnumerable<IEntity> OverlapCapsule(Vector3 a, Vector3 b, float radius);
         IEnumerable<IEntity> OverlapSphere(Vector3 position, float radius);
     }
 }
--- UnityPhysics.cs
+++ UnityPhysics.cs
@@ -25,6 +25,22 @@ namespace Physics
             }
         }
 
+        private static UnityVector3 Convert(Vector3 value)
+        {
+            return new UnityVector3(value.X, value.Y, value.Z);
+        }
+
+        public IEnumerable<IEntity> OverlapCapsule(Vector3 a, Vector3 b, float radius)
+        {
+            var length = UnityEngine.Physics.OverlapCapsuleNonAlloc(Convert(a), Convert(b), radius, _colliders);
+            for (var i = 0; i < length; i++)
+            {
+                _entities[i].Impl = _colliders[i].gameObject;
+            }
+
+            return _entities.Take(length);
+        }
+
         public IEnumerable<IEntity> OverlapSphere(Vector3 position, float radius)
         {
             var length = UnityEngine.Physics.OverlapSphereNonAlloc(Convert(position), radius, _colliders);
@@ -34,11 +50,6 @@ namespace Physics
             }
 
             return _entities.Take(length);
-        }
-
-        private static UnityVector3 Convert(Vector3 value)
-        {
-            return new UnityVector3(value.X, value.Y, value.Z);
         }
     }
 }
