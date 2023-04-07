b515a33ac096b73755da38310af96d97026f87ea
DESKTOP-UJRR6FS\Admin <tuduchoang@gmail.com>
2023-04-07 22:58:38 +0700

    2.2.0

---
+++ PhysicExtension.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using Entities;
+using Transforms;
+
+namespace Physics
+{
+    public static class PhysicExtension
+    {
+        public static IEnumerable<IEntity> OverlapCapsule(this IPhysics physics, ICapsule capsule, ITransform transform)
+        {
+            var localScale = transform.LocalScale;
+            return physics.OverlapCapsule(
+                transform.Position + capsule.Center,
+                capsule.Height / localScale.Y,
+                capsule.Radius / Math.Min(localScale.X, localScale.Z),
+                transform.Up
+            );
+        }
+
+        public static IEnumerable<IEntity> OverlapSphere(this IPhysics physics, float radius, ITransform transform)
+        {
+            var localScale = transform.LocalScale;
+            return physics.OverlapSphere(
+                transform.Position,
+                radius / Math.Min(Math.Min(localScale.X, localScale.Y), localScale.Z)
+            );
+        }
+    }
+}
